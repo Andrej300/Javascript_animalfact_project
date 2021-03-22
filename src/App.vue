b22e7bfa056535b0345ef16aac217ea5316daece
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2021-03-22 02:09:14 +0000

    added more data

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,23 @@ export default {
   data() {
     return {
    animals: [
-        {name: "snake", weight: 5},
-        {name: "dog", weight: 25},
+        {name:'Dog',weight:30, group:'Mammal', nutrition: 'Carnivorous'},
+        {name:'Cat', weight:5, group:'Mammal', nutrition: 'Carnivorous'},
+        {name:'Tiger',weight:200, group:'Mammal', nutrition: 'Carnivorous'},
+        {name:'Elephant',weight:6000, group:'Mammal', nutrition: 'Herbivorous'},
+        {name:'Chicken',weight:2, group:'Bird', nutrition: 'Herbivorous'},
+        {name:'Mouse',weight:1, group:'Mammal', nutrition: 'Omnivorous'},
+        {name:'Lion',weight:250, group:'Mammal', nutrition: 'Carnivorous'},
+        {name:'Monkey',weight:50, group:'Mammal', nutrition: 'Omnivorous'},
+        {name:'Giraffe',weight:800, group:'Mammal', nutrition:'Herbivorous'},
+        {name:'Snake',weight:510, group:'Fish', nutrition: 'Carnivorous'},
+        {name:'Dolphin',weight:160, group:'Fish', nutrition: 'Carnivorous'},
+        {name:'Crocodile',weight:400, group:'Reptiles', nutrition: 'Carnivorous'},
+        {name:'Eagle',weight:10, group:'Birds', nutrition: 'Carnivorous'},
+        {name:'Stork',weight:8, group:'Bird', nutrition: 'Carnivorous'},
+        {name:'Ant',weight:1, group:'Invertebrate', nutrition: 'Omnivorous'},
+        {name:'Horse',weight:550, group:'Mammal', nutrition: 'Herbivorous'}
+        
       ],
       selectedAnimal: null
     };
--- src/components/AnimalDetail.vue
+++ src/components/AnimalDetail.vue
@@ -1,12 +1,6 @@
 
 
-<template lang="html">
-    <div v-if='animal'>
-  <h2>{{ animal.name }}</h2>
-  <h3>Weight: {{ animal.weight }}</h3>
-  <h3>Group: {{ animal.group.toLocaleString() }}</h3>
-  </div>
-</template>
+
 
 <script>
 export default {
